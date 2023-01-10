88311836219012dae16f445e96b934c41c0de691
DanFilipe2000 <anielilipe90@gmail.com>
2023-01-09 22:17:17 -0300

    first commit

---
+++ resources/views/institute.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Templates</title>
+</head>
+<body>
+    <h2>Instituto de Ciência e Tecnologia de Franca</h2>
+</body>
+</html>
